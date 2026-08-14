d157903afdf32b8261ff553dd3c1678179a011ce
ramafauzi1986-hash <ramafauzi1986@gmail.com>
2026-08-14 15:15:13 +0700

    Link indicators to realizations for reports

--- app/Models/IndikatorKinerja.php
+++ app/Models/IndikatorKinerja.php
@@ -1,4 +1,4 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
-class IndikatorKinerja extends Model { protected $fillable=['sub_kegiatan_id','nama','target','satuan','sumber_data']; public function subKegiatan(){return $this->belongsTo(SubKegiatan::class);} }
+class IndikatorKinerja extends Model { protected $fillable=['sub_kegiatan_id','nama','target','satuan','sumber_data']; public function subKegiatan(){return $this->belongsTo(SubKegiatan::class);} public function realisasis(){return $this->hasMany(Realisasi::class,'indikator_kinerja_id');} }
