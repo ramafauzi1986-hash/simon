8d8cda112e6af49c39c53b757530fa328cfc46b6
ramafauzi1986-hash <ramafauzi1986@gmail.com>
2026-08-14 15:05:32 +0700

    Add Program controller

---
+++ app/Http/Controllers/ProgramController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\Program;
+use Illuminate\Http\Request;
+class ProgramController extends Controller {
+ public function index(){ $programs=Program::latest()->paginate(10); return view('programs.index',compact('programs')); }
+ public function store(Request $request){ $data=$request->validate(['kode'=>'nullable|string|max:50','nama'=>'required|string|max:255','target'=>'nullable|numeric','satuan'=>'nullable|string|max:50']); Program::create($data); return back()->with('success','Program berhasil ditambahkan.'); }
+ public function destroy(Program $program){ $program->delete(); return back()->with('success','Program berhasil dihapus.'); }
+}
