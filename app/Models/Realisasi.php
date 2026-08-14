4868e184b27b46e62a457ecbddac7d4eb59d5c79
ramafauzi1986-hash <ramafauzi1986@gmail.com>
2026-08-14 15:12:52 +0700

    Support evidence path on realization

--- app/Models/Realisasi.php
+++ app/Models/Realisasi.php
@@ -1,4 +1,4 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
-class Realisasi extends Model { protected $fillable=['indikator_kinerja_id','triwulan','nilai','keterangan','dokumen']; public function indikator(){return $this->belongsTo(IndikatorKinerja::class,'indikator_kinerja_id');} }
+class Realisasi extends Model { protected $fillable=['indikator_kinerja_id','triwulan','nilai','keterangan','dokumen','evidence_path']; public function indikator(){return $this->belongsTo(IndikatorKinerja::class,'indikator_kinerja_id');} }
