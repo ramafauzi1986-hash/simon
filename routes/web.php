c364f542779bee6a814e7058c417f8c663393e75
ramafauzi1986-hash <ramafauzi1986@gmail.com>
2026-08-14 15:05:46 +0700

    Add Program routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
-
+use App\Http\Controllers\ProgramController;
 Route::get('/', fn () => redirect('/dashboard'));
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/programs', [ProgramController::class, 'index'])->name('programs.index');
+Route::post('/programs', [ProgramController::class, 'store'])->name('programs.store');
+Route::delete('/programs/{program}', [ProgramController::class, 'destroy'])->name('programs.destroy');
