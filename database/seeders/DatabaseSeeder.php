818d536f47daad2f7f4f19baecc491149d31a3f8
ramafauzi1986-hash <ramafauzi1986@gmail.com>
2026-08-14 15:21:40 +0700

    Add initial SIMON admin account to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,16 +1,8 @@
 <?php
-
 namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 use App\Models\Program;
 use App\Models\Indikator;
-
-class DatabaseSeeder extends Seeder
-{
-    public function run(): void
-    {
-        $p = Program::create(['kode'=>'01','nama'=>'Program Penunjang Urusan Pemerintahan Daerah','target'=>100,'satuan'=>'%']);
-        Indikator::create(['program_id'=>$p->id,'nama'=>'Persentase Pemenuhan Pelayanan Administrasi DPRD','target'=>100,'satuan'=>'%']);
-    }
-}
+class DatabaseSeeder extends Seeder { public function run(): void { $p=Program::firstOrCreate(['kode'=>'01'],['nama'=>'Program Penunjang Urusan Pemerintahan Daerah','target'=>100,'satuan'=>'%']); Indikator::firstOrCreate(['program_id'=>$p->id,'nama'=>'Persentase Pemenuhan Pelayanan Administrasi DPRD'],['target'=>100,'satuan'=>'%']); User::updateOrCreate(['email'=>'admin@simon-setwan.local'],['name'=>'Administrator SIMON','password'=>Hash::make('Admin@12345'),'role'=>'admin']); } }
