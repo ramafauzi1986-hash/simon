97994bb8482c536f150c0d42e4d8cfbe8c51c743
ramafauzi1986-hash <ramafauzi1986@gmail.com>
2026-08-14 15:06:42 +0700

    Link SubKegiatan to Kegiatan

--- app/Models/SubKegiatan.php
+++ app/Models/SubKegiatan.php
@@ -1,4 +1,4 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
-class SubKegiatan extends Model { protected $fillable=['kode','nama','target','satuan']; }
+class SubKegiatan extends Model { protected $fillable=['kegiatan_id','kode','nama','target','satuan']; public function kegiatan(){return $this->belongsTo(Kegiatan::class);} }
