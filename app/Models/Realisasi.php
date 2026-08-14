d9b187c6b75f5bd05ea2f4ec544c769bd7e108db
ramafauzi1986-hash <ramafauzi1986@gmail.com>
2026-08-14 15:09:29 +0700

    Refine quarterly realization model

--- app/Models/Realisasi.php
+++ app/Models/Realisasi.php
@@ -1,10 +1,4 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
-
-class Realisasi extends Model
-{
-    protected $fillable = ['indikator_id', 'periode', 'nilai', 'keterangan', 'dokumen'];
-}
+class Realisasi extends Model { protected $fillable=['indikator_kinerja_id','triwulan','nilai','keterangan','dokumen']; public function indikator(){return $this->belongsTo(IndikatorKinerja::class,'indikator_kinerja_id');} }
