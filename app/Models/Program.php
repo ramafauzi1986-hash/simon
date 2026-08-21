9034e1bc649ef0c31bea0796a6ccef0ed1778d86
ramafauzi1986-hash <ramafauzi1986@gmail.com>
2026-08-21 17:10:30 +0700

    Bangun relasi Program ke Kegiatan

--- app/Models/Program.php
+++ app/Models/Program.php
@@ -1,4 +1,14 @@
 <?php
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
-class Program extends Model { protected $fillable=['kode','nama','target','satuan']; }
+
+class Program extends Model
+{
+    protected $fillable = ['kode', 'nama', 'target', 'satuan'];
+
+    public function kegiatans()
+    {
+        return $this->hasMany(Kegiatan::class);
+    }
+}
